982be766a0c95fa6ac826f42eed5ea3eae80c4cf
Freddie Yershon <github@freddieyershon.co.uk>
2026-09-20 20:58:15 +0100

    my head

--- scripts/finish.gd
+++ scripts/finish.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var label: Label = $Player/Label
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
@@ -7,4 +9,4 @@ func _on_body_entered(body: Node) -> void:
 	if body is CharacterBody2D:
 		var stopwatch = get_tree().current_scene
 		stopwatch.stop()
-		print("Finished in: ", stopwatch.get_formatted_time())
+		stopwatch.label.text = "Finished in: " + stopwatch.get_formatted_time()
